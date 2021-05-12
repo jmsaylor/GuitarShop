1f0b3f5bb79caae8efbe6bb1795971f485faab19
John <johnmsaylor@gmail.com>
2021-05-11 23:22:12 -0400

    Initial Commit

---
+++ src/com/johnsaylor/Guitar.java
@@ -0,0 +1,13 @@
+package com.johnsaylor;
+
+enum StringCount {SIX, TWELVE}
+
+public class Guitar {
+    StringCount strings;
+    boolean hasPickup;
+    String material;
+    double price;
+
+    public
+
+}
---
+++ src/com/johnsaylor/Main.java
@@ -0,0 +1,8 @@
+package com.johnsaylor;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
